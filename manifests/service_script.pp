93988c92fd4c4105f4ea764b8debb23628c7baf3
Peter Van Bouwel <peter.vanbouwel@ipc.be>
2014-11-20 16:32:31 +0100

    Alter service_script.pp to manage dependencies correclty

--- manifests/service_script.pp
+++ manifests/service_script.pp
@@ -6,35 +6,36 @@ define tomcat::service_script (
   $templates,
   $user,
 ) {
-  
-  
-  if $ensure == present {
-    $tomcat_service_ensure = $ensure
-    $tomcat_service_status = running
-  } else {
-    $tomcat_service_status = stopped
-    $tomcat_service_ensure = absent
-  }
-  
-  
-  
+   
   if ! $templates['servicescript.sh'] {
     $servicescript_content = template('tomcat/servicescript.sh.erb')
   } else {
     $servicescript_content = template($templates['servicescript.sh'])
   }
-  
-  file{ "/etc/init.d/tomcat_${instancename}.sh":
-    ensure   => $tomcat_service_ensure,
-    content  => $servicescript_content,
-    mode   => '0755',
-  }
-  service{ "tomcat_${instancename}.sh":
-    ensure => $tomcat_service_status,
-    enable => true,
-    require => [ File["${product_dir}/conf/server.xml"], 
-                 File["${product_dir}/bin/startup.sh"], 
-                 File["${product_dir}/conf/logging.properties"], 
-                 File["/etc/init.d/tomcat_${instancename}.sh"] ]
+  if $ensure == present {
+   file{ "/etc/init.d/tomcat_${instancename}.sh":
+     ensure   => present,
+     content  => $servicescript_content,
+     mode   => '0755',
+   }
+   service{ "tomcat_${instancename}.sh":
+     ensure => running,
+     enable => true,
+     require => File["/etc/init.d/tomcat_${instancename}.sh"]
+   }
+  }else {
+    if $ensure == present {
+	    file{ "/etc/init.d/tomcat_${instancename}.sh":
+	      ensure   => absent,
+	      content  => $servicescript_content,
+	      mode   => '0755',
+	      require => Service["tomcat_${instancename}.sh"]
+	    }
+
+	    service{ "tomcat_${instancename}.sh":
+	      ensure => stopped,
+	      enable => true,
+	    }
+    }
   }
 }
