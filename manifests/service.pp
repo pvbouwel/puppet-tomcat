731cb81d8488fc6505ebe4fb4d61293d99910693
Peter Van Bouwel <peter.vanbouwel@ipc.be>
2014-11-20 15:46:49 +0100

    Enable created services at boot time and start service after installation

--- manifests/service.pp
+++ manifests/service.pp
@@ -5,18 +5,24 @@ define tomcat::service (
   $templates,
   $user,
 ) {
+  
   if ! $templates['servicescript.sh'] {
-    file { "/etc/init.d/tomcat_${instancename}.sh":
-      ensure   => present,
-      content  => template('tomcat/servicescript.sh.erb'),
-      mode   => '0755',
-    }
+    $servicescript_content = template('tomcat/servicescript.sh.erb')
   } else {
-    file { "/etc/init.d/tomcat_${instancename}.sh":
-      ensure   => present,
-      content  => template($templates['servicescript.sh']),
-      mode   => '0755',
-    }
+    $servicescript_content = template($templates['servicescript.sh'])
   }
-
+  
+  file{ "/etc/init.d/tomcat_${instancename}.sh":
+    ensure   => present,
+    content  => $servicescript_content,
+    mode   => '0755',
+  }-> 
+  service{ "tomcat_${instancename}.sh":
+    ensure => running,
+    enable => true,
+    require => [ File["${product_dir}/conf/server.xml"], 
+                 File["${product_dir}/bin/startup.sh"], 
+                 File["${product_dir}/conf/logging.properties"], 
+                 File["/etc/init.d/tomcat_${instancename}.sh"] ]
+  }
 }
